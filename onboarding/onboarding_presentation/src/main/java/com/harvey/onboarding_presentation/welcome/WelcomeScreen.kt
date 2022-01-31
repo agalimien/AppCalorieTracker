7a354030570a1f77235e32ffa6d72270f6780b12
Harvey Tai <harvey.tai@scmp.com>
2022-01-31 10:01:04 +0800

    NavigationUtil ok

--- onboarding/onboarding_presentation/src/main/java/com/harvey/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/harvey/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -10,11 +10,15 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import com.harvey.core_ui.LocalSpacing
 import com.harvey.core.R
+import com.harvey.core.navigation.Route
+import com.harvey.core.util.UiEvent
 import com.harvey.onboarding_presentation.components.ActionButton
 
 
 @Composable
-fun WelcomeScreen() {
+fun WelcomeScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit
+) {
     val spacing = LocalSpacing.current
     Column(
         modifier = Modifier
@@ -31,7 +35,7 @@ fun WelcomeScreen() {
         Spacer(modifier = Modifier.height(spacing.spaceMedium))
         ActionButton(
             text = stringResource(id = R.string.next),
-            onClick = { /*TODO*/ },
+            onClick = { onNavigate(UiEvent.Navigate(Route.AGE)) },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
     }
