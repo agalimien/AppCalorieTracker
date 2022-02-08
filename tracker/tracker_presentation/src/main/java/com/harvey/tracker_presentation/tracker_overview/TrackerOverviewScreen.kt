a46a069c997a716fcd3ded3a1047a4189f08d01d
Harvey Tai <harvey.tai@scmp.com>
2022-02-08 10:16:17 +0800

    ExpandableMealUI

--- tracker/tracker_presentation/src/main/java/com/harvey/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/harvey/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -2,6 +2,7 @@ package com.harvey.tracker_presentation.tracker_overview
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -9,6 +10,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.harvey.core.util.UiEvent
 import com.harvey.core_ui.LocalSpacing
 import com.harvey.tracker_presentation.tracker_overview.components.DaySelector
+import com.harvey.tracker_presentation.tracker_overview.components.ExpandableMeal
 import com.harvey.tracker_presentation.tracker_overview.components.NutrientsHeader
 
 @Composable
@@ -40,6 +42,14 @@ fun TrackerOverviewScreen(
                     .padding(horizontal = spacing.spaceMedium)
             )
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+        items(state.meals) { meal ->
+            ExpandableMeal(
+                meal = meal,
+                onToggleClick = { viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClick(meal)) },
+                content = {  },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
