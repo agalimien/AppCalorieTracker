193f2e0d72d614e424cfb6dd054da03ac08f2090
Harvey Tai <harvey.tai@scmp.com>
2022-02-10 11:49:27 +0800

    SearchViewModel

---
+++ tracker/tracker_presentation/src/main/java/com/harvey/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.harvey.tracker_presentation.search
+
+import com.harvey.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
